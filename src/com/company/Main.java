5dd0eef33cdd09afc20c28a472398822aa184cfb
Hlibb <Astashev01@gmail.com>
2022-03-24 11:31:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("________WELCOME TO MONOBANK 2.0________");
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your surname:");
+        String surname = sc.nextLine();
+        System.out.println("Hi " + name + " " + surname + ", how are you doing?");
+        System.out.println("Let's start - enter your deposit amount ($):");
+        int amount = sc.nextInt();
+        System.out.println("Your yearly income: 2%");
+        double yic = 0.02; //yearlyIncomeCoefficient
+        if (amount > 20000) System.out.println("From now on, you are our gold member!");
+        System.out.println("Enter the duration of your deposit (years: 1, 2, 5):");
+        int duration = sc.nextInt();
+        System.out.println("Thanks for applying on deposit in uor bank!\n____________Please wait____________");
+        double income = duration * (yic * amount) + amount; //I don't want to use loop or condition, so I decided to make deposit without complicate percent
+        System.out.println("Congrats! In " + duration + " years you will have [" + income + "] to collect!");
+        System.out.println("Thanks for using our service! Have a nice day!");
+    }
+}
